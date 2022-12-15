652c3c34933b5a495a8c792a83ecfbc1995ec0bf
ITWILL <ITWILL@DESKTOP-E7QKFEG>
2022-12-15 11:40:11 +0900

    myPage.js - 캐시충전 기능 활성화 - 이존규

---
+++ sbs/src/main/resources/static/js/myPage.js
@@ -0,0 +1,24 @@
+/**
+ * member.js
+ * /membet/signup.html
+ */
+
+
+window.addEventListener('DOMContentLoaded', function() {
+
+    // 캐쉬충전 홈페이지로 이동
+    const btnCash = document.querySelector('#btnCash')
+    const form = document.querySelector('#form')
+    btnCash.addEventListener('click', function() {
+        const result = confirm('캐쉬 충전하시겠습니까?');
+        if (result) {
+            form.action = '/user/cash';
+            form.method = 'get';
+            form.submit();
+
+        }
+    })
+
+
+
+});
