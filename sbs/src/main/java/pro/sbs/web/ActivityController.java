8ec10367a269042baa969e36bdc2ce10a72aa3ae
uinmh <uinmh@192.168.1.4>
2022-12-20 22:49:40 +0900

    test- 장민석

--- sbs/src/main/java/pro/sbs/web/ActivityController.java
+++ sbs/src/main/java/pro/sbs/web/ActivityController.java
@@ -58,7 +58,6 @@ public class ActivityController {
     @PostMapping("/activity/update")
     public String update(ActivityUpdateDto dto) {
         log.info("update(dto = {})", dto);
- ////
         Integer activityId = activityService.update(dto);
 
         log.info("activityId = {}", activityId);
